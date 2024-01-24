a1ac839e94de4ca78c641ae798315dbc6ac5667f
zDeVolTz <vlad.pinti@gmail.com>
2024-01-25 00:41:20 +0200

    fix bug

--- main.js
+++ main.js
@@ -5,10 +5,12 @@ function createdTable(trValue, tdValue) {
     const customTable = document.querySelector('.table_custom');
     let valueTable = document.createElement("table");
     let tbody = document.createElement("tbody");
+    let count = 1;
     for (let i = 1; i <= trValue; i++) {
         let row = document.createElement('tr');
             for (let j = 1; j <= tdValue; j++){
-                 row.innerHTML += `<td>${i*j}</td>`;
+                row.innerHTML += `<td>${count}</td>`;
+                count++;
             }
         tbody.append(row);
     }
