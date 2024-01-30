e5c3e76520cdaecfc43443fde50853813b586260
zDeVolTz <vlad.pinti@gmail.com>
2024-01-30 09:19:03 +0200

    fix optimize code

--- main.js
+++ main.js
@@ -1,20 +1,18 @@
 const smileImg = document.querySelectorAll('.smile-gif');
 const smileBlock = document.querySelector('.smile-gif-container');
 
-(function() {
-    smileImg.forEach((element) => {
-        const divCount = document.createElement('div');
-        divCount.className = "count-block";
-        divCount.innerHTML = `<p class="count">0</p>`;
-        divCount.insertBefore(element, divCount.firstChild);
-        smileBlock.append(divCount);
+smileImg.forEach((element) => {
+    const divCount = document.createElement('div');
+    divCount.className = "count-block";
+    divCount.innerHTML = `<p class="count">0</p>`;
+    divCount.insertBefore(element, divCount.firstChild);
+    smileBlock.append(divCount);
+    element.addEventListener('click', () => {
+        const count = element.nextElementSibling;
+        let value = +count.innerHTML;
+        count.innerHTML = `${value + 1}`;
     });
-}());
+});
 
-smileImg.forEach((element) => (element.addEventListener('click', () => {
-   const count = element.nextElementSibling;
-   let value = +count.innerHTML;
-   count.innerHTML = `${value + 1}`;
-})));
 
 
