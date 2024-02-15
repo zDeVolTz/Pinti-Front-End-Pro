9ce5270efd4c8a855fbf0f7847928d4a371d2916
zDeVolTz <vlad.pinti@gmail.com>
2024-02-16 00:55:02 +0200

    add script

--- main.js
+++ main.js
@@ -2,49 +2,49 @@ const nameBrowser = document.querySelector(".titleBrowser");
 const verBrowser = document.querySelector(".verBrowser");
 const checkboxes = document.querySelectorAll('input[type="checkbox"]');
 const imgBlock = document.getElementById('imageBrowser');
+const brouwserList = {
+    opera : {
+            value : "Opera",
+            reg : /opr|opera|mac.*opt/i,
+            regVersion : /opr\/([0-9.]+)|opera\/([0-9.]+)/i,
+            urlImg : "./img/opera.jpg"           
+            },
+    edge :  {
+            value :"Microsoft Edge",
+            reg : /edg/i,
+            regVersion : /edg\/([0-9.]+)/i,
+            urlImg : "https://i.kym-cdn.com/photos/images/original/001/858/273/edd"
+            },
+    firefox : {
+            value : "Firefox",
+            reg : /firefox|mac.*fxios/i,
+            regVersion : /firefox\/([0-9.]+)/i,
+            urlImg : "https://images-ng.pixai.art/images/orig/0da0bb59-fdf7-4712-a74f-f16977d44877"
+            },
+    chrome : {
+            value : "Google Chrome",
+            reg : /chrome|mac.*gsa|mac.*crios/i,
+            regVersion : /chrome\/([0-9.]+)/i,
+            urlImg: "./img/google.jpg"
+            },
+    safari : {
+            value : "Safari",
+            reg : /safari/i,
+            regVersion : /safari\/([0-9.]+)/i,
+            urlImg : "https://preview.redd.it/safari-is-such-a-pain-v0-djbncqoaz9va1.jpg?auto=webp&s=8ac04336a33e3933d2c85133ef867074446f3d32"
+            },
+    default : "Хто ти монстр ?:)"
+}
 
-fnBrowserDetect()
+
+fnBrowserDetect(brouwserList)
 selectCheckboxesFromUrl();
 
-function fnBrowserDetect(){
+function fnBrowserDetect(brouwserList){
                  
     let userAgent = navigator.userAgent;
     let browserName;
     let browserVersion;
-    const brouwserList = {
-        opera : {
-                value : "Opera",
-                reg : /opr|opera|mac.*opt/i,
-                regVersion : /opr\/([0-9.]+)|opera\/([0-9.]+)/i,
-                urlImg : "./img/opera.jpg"           
-                },
-        edge :  {
-                value :"Microsoft Edge",
-                reg : /edg/i,
-                regVersion : /edg\/([0-9.]+)/i,
-                urlImg : "https://i.kym-cdn.com/photos/images/original/001/858/273/edd"
-                },
-        firefox : {
-                value : "Firefox",
-                reg : /firefox|mac.*fxios/i,
-                regVersion : /firefox\/([0-9.]+)/i,
-                urlImg : "https://images-ng.pixai.art/images/orig/0da0bb59-fdf7-4712-a74f-f16977d44877"
-                },
-        chrome : {
-                value : "Google Chrome",
-                reg : /chrome|mac.*gsa|mac.*crios/i,
-                regVersion : /chrome\/([0-9.]+)/i,
-                urlImg: "./img/google.jpg"
-                },
-        safari : {
-                value : "Safari",
-                reg : /safari/i,
-                regVersion : /safari\/([0-9.]+)/i,
-                urlImg : "https://preview.redd.it/safari-is-such-a-pain-v0-djbncqoaz9va1.jpg?auto=webp&s=8ac04336a33e3933d2c85133ef867074446f3d32"
-                },
-        default : "Хто ти монстр ?:)"
-    }
-
     
         if(brouwserList.opera.reg.test(userAgent)){
         browserName = brouwserList.opera.value;
